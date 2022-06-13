2850130a5f80ff260ac9dea8547dd82c6e004f0d
oliviercagnat <oliviercagnat@gmail.com>
2022-06-13 16:34:49 +0900

    finished app

---
+++ script.js
@@ -0,0 +1,53 @@
+const smallCups = document.querySelectorAll('.cup-small');
+const cupsList = document.querySelector('.cups');
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+updateBigCup();
+
+smallCups.forEach((cup, index) => {
+  cup.addEventListener('click', () => hightlightCups(index));
+});
+
+function hightlightCups(index) {
+  if ((smallCups[index].classList.contains('full')
+  && smallCups[index] === cupsList.lastElementChild)
+  || (smallCups[index].classList.contains('full')
+  && !smallCups[index].nextElementSibling.classList.contains('full'))
+  ) {
+    index--;
+  }
+
+  smallCups.forEach((cup, index2) => {
+    if (index2 <= index) {
+      cup.classList.add('full');
+    } else {
+      cup.classList.remove('full');
+    }
+  });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll('.cup-small.full').length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = 'hidden';
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = 'visible';
+    percentage.style.height = `${fullCups / totalCups * 330}px`;
+    percentage.innerText = `${fullCups / totalCups * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = 'hidden';
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = 'visible';
+    liters.innerText = `${2 - (250 * fullCups / 1000)}l`;
+  }
+}
